494fbe9b3125d702282140b58fd95a5375b824f4
Wade Phillips <phillips.acupuncture@gmail.com>
2021-01-21 05:36:22 -0900

    wip

---
+++ Tests/BowlingGameTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use PHPUnit\Framework\TestCase;
+
+class BowlingGameTest extends TestCase
+{
+  /** @test */
+  public function itTests()
+  {
+    $this->assertTrue(true);
+  }
+
+}
