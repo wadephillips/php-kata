1134de494baa28ad27f2fb5ef8f0a589605cb077
Wade Phillips <phillips.acupuncture@gmail.com>
2021-02-13 07:33:41 -0900

    wip

--- Tests/BowlingGameTest.php
+++ Tests/BowlingGameTest.php
@@ -3,13 +3,65 @@
 namespace App;
 
 use PHPUnit\Framework\TestCase;
+use function range;
 
 class BowlingGameTest extends TestCase
 {
   /** @test */
-  public function itTests()
+  public function itScoresAGutterGameAsZero()
   {
-    $this->assertTrue(true);
+    $game = new BowlingGame();
+
+    foreach (range(1,20) as $turn) {
+      $game->roll(0);
+    }
+
+    $this->assertEquals(0, $game->score());
   }
+  /** @test */
+  public function itScoresAOneOnEveryRollAsTwenty()
+  {
+    $game = new BowlingGame();
+
+    foreach (range(1,20) as $turn) {
+      $game->roll(1);
+    }
+
+    $this->assertEquals(20, $game->score());
+  }
+
+  /** @test */
+  public function itAwardsAOneRollBonusForASpare()
+  {
+    $game = new BowlingGame();
+
+    $game->roll(5);
+    $game->roll(5);
+
+    $game->roll(6);
+    foreach (range(4,20) as $turn) {
+      $game->roll(0);
+    }
+
+    $this->assertEquals(22, $game->score());
+  }
+
+  /** @test */
+  public function itAwardsATwoRollBonusForAStrike()
+  {
+    $game = new BowlingGame();
+
+    $game->roll(10);
+
+    $game->roll(5);
+
+    $game->roll(4);
+    foreach (range(4,20) as $turn) {
+      $game->roll(0);
+    }
+
+    $this->assertEquals(28, $game->score());
+  }
+
 
 }
--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -1,10 +1,73 @@
 <?php
 
-
 namespace App;
 
+use function array_sum;
+use function range;
+use function var_dump;
 
 class BowlingGame
 {
+
+  const TOTAL_FRAMES = 10;
+
+
+  private $rolls = [];
+
+
+  public function score(): int
+  {
+    //it keeps track of the score of a bowling game
+    $score = 0;
+    $roll = 0;
+    // there are 10 frames with 21 potential rolls
+    foreach (range(1, self::TOTAL_FRAMES) as $frame) {
+
+      //if first roll is 10 award strike bonus and proceed to next iteration
+      if ($this->isStrike($roll)) {
+        $score += 10 + $this->awardStrikeBonus($roll);
+        $roll += 2;
+        continue;
+      }
+
+      $score += $this->rolls[$roll] + $this->rolls[$roll + 1];
+
+
+      if ($this->isSpare($roll)){
+        $score += $this->awardSpareBonus($roll);
+      }
+      $roll += 2;
+    }
+    //if first roll of a frame = 10 add the next 2 rolls to to the total score
+
+    return $score;
+  }
+
+  public function roll(int $pins)
+  {
+    $this->rolls[] = $pins;
+    if ($pins === 10) $this->rolls[] = 0;
+    var_dump($this->rolls); //todo resume: i think strikes are being added to the rolls correcty now but we're still not adding up correctly
+  }
+
+  private function awardSpareBonus(int $roll)
+  {
+    return $this->rolls[$roll+2];
+  }
+
+  private function awardStrikeBonus($roll)
+  {
+    return $this->rolls[$roll+1] + $this->rolls[$roll+2];
+  }
+
+  private function isSpare($roll)
+  {
+    return $this->rolls[$roll] + $this->rolls[$roll + 1] === 10;
+  }
+
+  private function isStrike($roll)
+  {
+    return $roll % 2 === 0 && $this->rolls[$roll] === 10;
+  }
 
 }
